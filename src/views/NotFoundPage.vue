e6e2f828e02ca89ac31ec9141531cef0bd68b16f
Varunkumar0812 <varunkumarceg@gmail.com>
2024-08-09 16:36:18 +0530

    Alignment fixes

--- src/views/NotFoundPage.vue
+++ src/views/NotFoundPage.vue
@@ -1,3 +1,6 @@
+<script setup lang="ts">
+</script>
+
 <template>
     <div class="flex justify-center items-center flex-wrap min-h-screen">
         <h1 class=" bg-red-100 text-8xl w-full text-center text-red-700 py-40">404</h1>
@@ -5,8 +8,5 @@
         <router-link to="/" class="w-full text-center underline text-lime-700">Go to Home</router-link>
     </div>
 </template>
-
-<script setup>
-</script>
 
 <style scoped></style>
